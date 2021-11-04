bc97ec198bf66821149e8dcea0c906d4905c6a7c
Patrick Sullivan <psullivan6@gmail.com>
2021-11-04 12:18:43 -0400

    Added viewports to storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,43 @@
+const viewports = {
+  largeDesktopFS: {
+    name: 'Large Desktop (Fullscreen)',
+    styles: {
+      width: '1920px',
+      height: '1080px',
+    },
+  },
+  mediumDesktopFS: {
+    name: 'Medium Desktop (Fullscreen)',
+    styles: {
+      width: '1680px',
+      height: '1050px',
+    },
+  },
+  largeDesktop: {
+    name: 'Large Desktop',
+    styles: {
+      width: '1920px',
+      height: '1019px',
+    },
+  },
+  mediumDesktop: {
+    name: 'Medium Desktop',
+    styles: {
+      width: '1680px',
+      height: '989px',
+    },
+  },
+};
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  viewport: {
+    viewports,
+  },
+};
